4a8282b1710d0d580387d213e10dcf6233af0dcd
U-DESKTOP-U9AHVJ7\mitoh_n10j3vl <y_nomura.dev@outlook.jp>
2026-04-09 09:53:08 +0900

    feat:work11に積算機能を追加

--- lec01/work11.c
+++ lec01/work11.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(void) {
-    int n, m;
+    int n, m, total;
     scanf("%d", &n);
     if (n < 0) return 1;
+    total = n;
     while (1) {
         scanf("%d", &m);
         if (m < 0){
             printf("%d\n", n);
+            printf("%d\n", total);
             break;
             }
         n += m;
+        total *= m;
     }
 }
