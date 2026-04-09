9824e2240c9085c1fe068ed62da449e7125ebe7e
U-DESKTOP-U9AHVJ7\mitoh_n10j3vl <y_nomura.dev@outlook.jp>
2026-04-09 11:33:37 +0900

    add:work14

---
+++ lec01/work14.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main(void){
+
+    char *s1 = "Hello";
+    char *s2 = "Good morning!!";
+    char *s3 = "2026";
+
+    printf("s1:%s\n", s1);
+    printf("s2:%s\n", s2);
+    printf("s3:%s\n", s3);
+
+    return 0;
+
+}
